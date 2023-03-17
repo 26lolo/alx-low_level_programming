10514941c6478abf61bb391a797ecf5cc0d68700
Lolo254 <odhiambo.laureen@gmail.com>
2023-03-17 05:19:44 -0700

    0x00. C- Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * main -  prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19" to standard error
+ * main -  prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
  * Return: Always 1
  */
 
@@ -10,5 +10,5 @@ int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-}	
+}
 
