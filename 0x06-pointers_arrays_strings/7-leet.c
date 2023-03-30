c9410afa49a6433c25165e61112ae93b37d1fdae
Lolo254 <odhiambo.laureen@gmail.com>
2023-03-29 17:32:45 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,25 @@
 #include "main.h"
 
 /**
- * *leet - encodes a string into 1337
+ * leet - encodes a string into 1337
+ * @n: input
  *
+ * Return: the value of n
  */
 
-char *leet(char *)
+char *leet(char *n)
 {
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (j = 0; j < 10; j++)
+	{
+		if (n[i] == s1[j])
+		{
+			n[i] = s2[j];
+		}
+	}
+	return (n);
 
 }
