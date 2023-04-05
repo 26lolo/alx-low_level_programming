10dab04f172c1ddad80818465af49545d1baef4d
Lolo254 <odhiambo.laureen@gmail.com>
2023-04-05 02:20:52 -0700

    C - Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s - string to be returned
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	char s;
+
+	puts(s);
+}
