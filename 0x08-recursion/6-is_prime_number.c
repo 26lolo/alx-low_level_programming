32bd8a88cf501e7f27010987b22d0f58de844c8e
Lolo254 <odhiambo.laureen@gmail.com>
2023-04-05 20:22:11 -0700

    C - Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -26,10 +26,8 @@ int actual_prime(int n, int i)
 
 int is_prime_number(int n)
 {
-	int i;
-	int s = actual_prime(n, i - 1);
-
-	if (s <= 1)
+	if (n <= 1)
 		return (0);
-	return (1);
+	else
+		return (actual_prime(n, 1));
 }
