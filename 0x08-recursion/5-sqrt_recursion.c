ef2ad0a7fa95f1987f28462c8e6de9d831ab3c42
Lolo254 <odhiambo.laureen@gmail.com>
2023-04-05 19:25:09 -0700

    C -Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,16 +4,17 @@
 /**
  * _sqrt - calculates natural squareroot
  * @n: integer to be calculated
- * @i: iteraye number
+ * @i: iterate number
  *
  * Return: natural squareroot
  */
 
 int _sqrt(int n, int i)
 {
-	int sqrt = i * i
-		if (sqrt > n)
-			return (-1);
+	int sqrt = i * i;
+
+	if (sqrt > n)
+		return (-1);
 	if (sqrt == n)
 		return (i);
 	return (_sqrt(n, i + 1));
