59c8a1868e247c09caa6b7458b834a347c825b3d
Lolo254 <odhiambo.laureen@gmail.com>
2023-04-12 17:53:36 -0700

    0x0B. C - malloc, free

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words separated by spaces
+ * @str: string to be split
+ *
+ * Return: pointer to an array of strings, otherwise NULL
+ */
+
+char **strtow(char *str)
+{
+
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+# 0x0B-malloc_free #
+
+0. Float like a butterfly, sting like a bee; function that creates an array of chars, and initializes it with a specific char.
+
