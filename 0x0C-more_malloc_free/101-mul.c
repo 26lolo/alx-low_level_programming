5f7052e789df47ba8c5e610f7da9a6b0a4d84758
Lolo254 <odhiambo.laureen@gmail.com>
2023-04-13 19:57:22 -0700

    0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two positive numbers
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	unsigned num1, num2;
+	int mul;
+
+	mul = num1 * num2
+
+		printf("%d\n", mul);
+	return (0);
+}
